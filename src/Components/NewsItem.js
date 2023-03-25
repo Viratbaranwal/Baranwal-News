cbd02774c27b895d520dac49e039a57571083b0f
Virat Raj <viratraj@Virats-MacBook-Air.local>
2023-03-25 12:05:36 +0530

    target

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,12 +5,12 @@ export class NewsItem extends Component {
     let {title, description, imgurl, newsUrl} = this.props;
     return (
         <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
         <img src={!imgurl?"https://www.enbridg.com/wp-content/themes/eikra/assets/img/noimage-420x273.jpg":imgurl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{description}...</p>
-          <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+          <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
         </div>
       </div>
       </div>
